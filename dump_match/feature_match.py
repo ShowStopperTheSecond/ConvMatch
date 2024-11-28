81ebc6da268bf6771d9fbbc0962adab881108800
javid <norouzi.javid@protonmail.com>
2024-11-28 20:18:49 +0330

    double desc matching fixed_bug

--- dump_match/feature_match.py
+++ dump_match/feature_match.py
@@ -25,6 +25,39 @@ def computeNN(desc_ii, desc_jj):
 
 
 
+# def sub_desc_match(desc1, desc2, desc_size):
+#     splitted_desc1 = np.split(desc1, desc_size,1)
+#     splitted_desc2 = np.split(desc2, desc_size,1)
+#     hist = np.zeros(shape=(len(desc1), len(desc2)))
+#     for d1, d2 in zip (splitted_desc1, splitted_desc2):
+#         idx_sort, ratio_test, mutual_nearest = computeNN(d1, d2)
+#         hist[idx_sort[0][mutual_nearest], idx_sort[1][mutual_nearest]] +=1
+#     return hist
+    
+# def multi_sub_desc_match(desc1, desc2, desc_size, min_match):
+#     hists = []
+#     for d1, d2 in zip(desc1, desc2):
+#         hist = sub_desc_match(d1, d2, desc_size)
+#         hists.append(hist)
+#     final_matches = hists[0]>min_match
+#     if len(hists) >1: 
+#         for h in hists[1:]:
+#             final_matches = np.logical_and(final_matches, h>min_match)
+            
+#     final_matches = np.argwhere(final_matches)
+#     idx_sort = np.c_[np.arange(len(d1)), np.random.choice(np.arange(len(d2)), size=len(d1), replace=True)]
+#     idx_sort[final_matches[:, 0], 1] = final_matches[:, 1]
+#     idx_sort = [idx_sort[:, 0], idx_sort[:, 1]]
+#     mutual_matches =  np.zeros(len(idx_sort[0]), np.bool_)
+#     mutual_matches[final_matches[:, 0]]=True
+#     ratio_test = np.logical_not(mutual_matches).astype('float32')
+#     return idx_sort, ratio_test, mutual_matches
+    
+
+
+
+
+
 def sub_desc_match(desc1, desc2, desc_size):
     splitted_desc1 = np.split(desc1, desc_size,1)
     splitted_desc2 = np.split(desc2, desc_size,1)
@@ -45,15 +78,20 @@ def multi_sub_desc_match(desc1, desc2, desc_size, min_match):
             final_matches = np.logical_and(final_matches, h>min_match)
             
     final_matches = np.argwhere(final_matches)
-    idx_sort = np.c_[np.arange(len(d1)), np.random.choice(np.arange(len(d2)), size=len(d1), replace=True)]
-    idx_sort[final_matches[:, 0], 1] = final_matches[:, 1]
-    idx_sort = [idx_sort[:, 0], idx_sort[:, 1]]
-    mutual_matches =  np.zeros(len(idx_sort[0]), np.bool_)
-    mutual_matches[final_matches[:, 0]]=True
-    ratio_test = np.logical_not(mutual_matches).astype('float32')
-    return idx_sort, ratio_test, mutual_matches
+    # idx_sort = np.c_[np.arange(len(d1)), np.random.choice(np.arange(len(d2)), size=len(d1), replace=True)]
+    # idx_sort[final_matches[:, 0], 1] = final_matches[:, 1]
+    # idx_sort = [idx_sort[:, 0], idx_sort[:, 1]]
+    idx_sort = [final_matches[:, 0], final_matches[:, 1]]
+    # mutual_matches =  np.zeros(len(idx_sort[0]), np.bool_)
+    # mutual_matches[final_matches[:, 0]]=True
+    # ratio_test = mutual_matches.astype('float32')
+
+    return idx_sort, None, None
     
 
+    
+    
+    
     
     
     
