4010db54de9d4c05f90834778fe632e183bc2daa
javid <norouzi.javid@protonmail.com>
2024-11-29 06:20:40 +0330

    double desc matching fixed_bug

--- dump_match/sequence.py
+++ dump_match/sequence.py
@@ -132,7 +132,6 @@ class Sequence(object):
         idx_sort, ratio_test, mutual_nearest = nn_info["idx_sort"], nn_info["ratio_test"], nn_info["mutual_nearest"]
         x1 = x1[idx_sort[0],:]
         x2 = x2[idx_sort[1],:]
-        print(len(x1), len(x2))
         xs = np.concatenate([x1, x2], axis=1).reshape(1,-1,4)
         geod_d = get_episym(x1, x2, dR, dt)
         ys = geod_d.reshape(-1,1)
