5b7e0f3894a061c62420e573038076a70f969693
javid <norouzi.javid@protonmail.com>
2024-12-02 18:47:27 +0330

    OR for distinct descriptors doesn't work well, AND is good

--- dump_match/feature_match.py
+++ dump_match/feature_match.py
@@ -80,8 +80,8 @@ def multi_sub_desc_match(desc1, desc2, desc_size, min_match):
     final_matches = hists[0]>min_match
     if len(hists) >1: 
         for h in hists[1:]:
-            # final_matches = np.logical_and(final_matches, h>min_match)
-            final_matches = np.logical_or(final_matches, h>min_match)
+            final_matches = np.logical_and(final_matches, h>min_match)
+            # final_matches = np.logical_or(final_matches, h>min_match)
 
             
     final_matches = np.argwhere(final_matches)
